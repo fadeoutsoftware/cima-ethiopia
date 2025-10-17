fd0e5ed0bb3f87cb11ce16ddef1c9e9551617a75
Marco Menapace <m.menapace@fadeout.it>
2025-10-17 11:24:43 +0200

    launcher script added

---
+++ docker/launcher.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+cd $CONDA_PATH 
+cd bin 
+source activate shybox_base_libraries
+
+export PYTHONPATH="${PYTHONPATH}:/app/shybox"
+
+cd /home/continuumuser/workdir
+
+jupyter notebook \
+     --ip=0.0.0.0 \
+     --port=8888 \
+     --no-browser \
+     --NotebookApp.token='' \
+     --NotebookApp.notebook_dir=/home/continuumuser/workdir

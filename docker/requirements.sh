26328ac1c998b67e3be269bf74f7378d19f517af
Marco Menapace <m.menapace@fadeout.it>
2025-10-17 17:29:49 +0200

    Requirements script and source fix

--- docker/requirements.sh
+++ docker/requirements.sh
@@ -1,8 +1,9 @@
+#!/bin/bash -e
 echo " ==================================================================================="
 echo " ==> START ..."
 echo " ==> Creating virual environment ..."
 
-python3 -m venv .venv
+/usr/bin/python3.12 -m venv .venv
 source .venv/bin/activate
 python3 -m pip install --upgrade pip
 
@@ -11,8 +12,8 @@ python3 -m pip install -r requirements_aics_iwrm.txt
 
 echo " ==> Configuring Jupyter Lab ..."
 
-jupyter nbextension install --py widgetsnbextension --sys-prefix \
- && jupyter nbextension enable --py widgetsnbextension --sys-prefix
+jupyter labextension install --py widgetsnbextension \
+ && jupyter labextension enable --py widgetsnbextension 
 
 echo " ==> DONE - Bye Bye"
 echo " ==================================================================================="

236ac8b4e0227cabe126814e92d9cacf5701c3a6
Alberto Tasso <a.tasso@fadeout.it>
2025-10-31 16:55:26 +0100

    Fixed IPyhon reference error

--- docker/requirements.sh
+++ docker/requirements.sh
@@ -12,8 +12,8 @@ python3 -m pip install -r requirements_aics_iwrm.txt
 
 echo " ==> Configuring Jupyter Lab ..."
 
-jupyter labextension install --py widgetsnbextension \
- && jupyter labextension enable --py widgetsnbextension 
+# jupyter labextension install --py widgetsnbextension \
+# && jupyter labextension enable --py widgetsnbextension 
 
 echo " ==> DONE - Bye Bye"
 echo " ==================================================================================="

0c9d5f290de1e8eab509d6871acbf282117e7c30
Marco Menapace <m.menapace@fadeout.it>
2025-11-20 12:41:23 +0100

    Renaming project

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ sys.path.insert(0, os.path.abspath('.'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'Cima AICS IWRM'
+project = 'Continuum Training Environment documentation'
 copyright = 'CIMA - 2025'
 author = ''
 
